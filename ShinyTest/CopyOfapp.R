16395a2cf3a9f1f3b8b8997e479fc1b8a89a136b
Jenny HY Lin <jennyhylin@gmail.com>
2023-03-31 22:22:58 +1300

    Shiny app trial in progress

---
+++ ShinyTest/CopyOfapp.R
@@ -0,0 +1,76 @@
+library(shiny)
+#source("Chembl_mols_new.R")
+
+
+# Define UI for app ----
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Small molecules in ChEMBL database"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      
+      # Input: Select box to choose physicochemical features
+      selectInput("Physicochemical properties", label = h3("Select box"), 
+                  choices = list("QED weighted scores" = QED_Weighted, 
+                                 "Polar surface area" = Polar_Surface_Area, 
+                                 "Molecular weight" = Molecular_Weight), 
+                  selected = QED_Weighted),
+      
+      # Input: Radio buttons to choose max phases ----
+      # radioButtons("radio", label = h3("Max phase"),
+      #              choices = list("phase 0" = 0, "phase 1" = 1, "phase 2" = 2, "phase 3" = 3, "phase 4" = 4), 
+      #              selected = 0),
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # *Output function in ui to place reactive object in Shiny app
+      # Output: Lollipop Plot ----
+      plotOutput(outputId = "lolliPlot")
+      
+    )
+  )
+)
+
+# Define server logic ----
+server <- function(input, output) {
+  
+  # render* function in server to tell Shiny how to build objects
+  # Surround R expressions by {} in each render* function
+  # Save render* expressions in the output list, with one entry for 
+  # each reactive object in the app
+  # Create reactivity by including an input value in a render* expression
+  
+  # Sample code from widget gallery
+  output$lolliPlot <- renderPlot({ 
+    
+    #input$radio
+
+    
+    })
+  
+  # Sample app code starts here
+  # output$distPlot <- renderPlot({
+  #   
+  #   # Insert dataset below?
+  #   x    <- faithful$waiting
+  #   bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  #   
+  #   hist(x, breaks = bins, col = "#75AADB", border = "orange",
+  #        xlab = "Waiting time to next eruption (in mins)",
+  #        main = "Histogram of waiting times")
+  #   
+  # })
+  
+}
+
+# Create/run Shiny app ----
+shinyApp(ui = ui, server = server)
+
--- ShinyTest/app.R
+++ ShinyTest/app.R
@@ -1,74 +1,82 @@
 library(shiny)
-#source("Chembl_mols_new.R")
+library(tidyverse)
+library(ggplot2)
 
+
+# Function to plot variables against max phases of small molecules
+dfBoxplot <- function(var) {
+  
+  chembl <- read_csv("chembl_mols_new.csv")
+  #label <- rlang::englue("{{var}} vs. Max Phases of small molecules")
+  
+  chembl %>% 
+    select(`Max Phase`, {{ var }}) %>% 
+    ggplot(aes(x = `Max Phase`, y = {{ var }})) +
+    geom_boxplot(aes(group = cut_width(`Max Phase`, 0.25), 
+                     colour = `Max Phase`), outlier.alpha = 0.2)
+    #labs(title = label)
+}
 
 # Define UI for app ----
-ui <- fluidPage(
+ui <- pageWithSidebar(
   
   # App title ----
-  titlePanel("Small molecules in ChEMBL database"),
+  headerPanel("Small molecules in ChEMBL database"),
   
-  # Sidebar layout with input and output definitions ----
-  sidebarLayout(
-    
-    # Sidebar panel for inputs ----
+    # Sidebar layout with input and output definitions ----
     sidebarPanel(
+    
+    
       
       # Input: Select box to choose physicochemical features
-      selectInput("Physicochemical properties", label = h3("Select box"), 
-                  choices = list("QED weighted scores" = QED_Weighted, 
-                                 "Polar surface area" = Polar_Surface_Area, 
-                                 "Molecular weight" = Molecular_Weight), 
-                  selected = QED_Weighted),
-      
-      # Input: Radio buttons to choose max phases ----
-      # radioButtons("radio", label = h3("Max phase"),
-      #              choices = list("phase 0" = 0, "phase 1" = 1, "phase 2" = 2, "phase 3" = 3, "phase 4" = 4), 
-      #              selected = 0),
+      selectInput("variable", "Variable:", 
+                  c("QED weighted scores" = "QED Weighted", 
+                    "Polar surface area" = "Polar Surface Area",
+                    "Molecular weight" = "Molecular Weight")),
+    
+      # Input: Checkbox for choosing to include outliers or not ----
+      #checkboxInput("outliers", "Show outliers", TRUE)
       
     ),
     
     # Main panel for displaying outputs ----
     mainPanel(
       
-      # *Output function in ui to place reactive object in Shiny app
-      # Output: Lollipop Plot ----
-      plotOutput(outputId = "lolliPlot")
+      # Output: Formatted text for caption ----
+      # h4(textOutput("caption")),
+      
+      # Output: Plot for the requested variable against max phases ----
+      plotOutput("chemblPlot")
       
     )
-  )
+  
 )
 
 # Define server logic ----
+
+
+#chemblData$`Max Phase` <- factor(chemblData$`Max Phase`, labels = c("zero", "one", "two", "three", "four"))
+
 server <- function(input, output) {
   
+
   # render* function in server to tell Shiny how to build objects
   # Surround R expressions by {} in each render* function
   # Save render* expressions in the output list, with one entry for 
   # each reactive object in the app
   # Create reactivity by including an input value in a render* expression
   
-  # Sample code from widget gallery
-  output$lolliPlot <- renderPlot({ 
-    
-    #input$radio
+  
+  # Change code below ?linking selection input to column names
+  selectData <- reactive({input$variable})
+  
 
+  # Generate plot for requested variable against max phases
+  output$chemblPlot <- renderPlot({
     
-    })
-  
-  # Sample app code starts here
-  # output$distPlot <- renderPlot({
-  #   
-  #   # Insert dataset below?
-  #   x    <- faithful$waiting
-  #   bins <- seq(min(x), max(x), length.out = input$bins + 1)
-  #   
-  #   hist(x, breaks = bins, col = "#75AADB", border = "orange",
-  #        xlab = "Waiting time to next eruption (in mins)",
-  #        main = "Histogram of waiting times")
-  #   
-  # })
-  
+    dfBoxplot(selectData())
+    
+  })
 }
 
 # Create/run Shiny app ----
